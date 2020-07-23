337e0c2885ba3d9e13c607eb83e444baef8d63ea
rodiguembah13 <rodriguembah13@gmail.com>
2020-07-23 12:55:15 +0100

    version 04

--- resources/views/front/pages/about-slider.blade.php
+++ resources/views/front/pages/about-slider.blade.php
@@ -4,13 +4,13 @@
             <div class="col-md-10">
                 <div class="hero-content" style="text-align: center">
                     <h2 class="hero-title" data-aos="zoom-in">About Us</h2>
-                    <h3 data-aos="zoom-in">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis in tenetur doloremque, maiores doloribus officia iste. Dolores.</h3>
+                    <h3 data-aos="zoom-in"></h3>
                                        
                    {{-- <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
                     <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
                     --}}
                     <br>
-                    <a data-aos="zoom-in-down" class="btn btn-primary btn-lg" style="text-align: center;" href="#" target="_blank" role="button">Learn More</a>
+{{--                    <a data-aos="zoom-in-down" class="btn btn-primary btn-lg" style="text-align: center;" href="#" target="_blank" role="button">Learn More</a>--}}
                 </div>
             </div>
             <div class="col-md-4"></div>
--- resources/views/front/pages/blog-slider.blade.php
+++ resources/views/front/pages/blog-slider.blade.php
@@ -4,13 +4,13 @@
             <div class="col-md-10">
                 <div class="hero-content" style="text-align: center" data-aos="fade-in">
                     <h2 class="hero-title">Blog Posts</h2>
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis in tenetur doloremque, maiores doloribus officia iste. Dolores.</h3>
+                    <h3></h3>
                                        
                    {{-- <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
                     <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
                     --}}
                     <br>
-                    <a class="btn btn-primary btn-lg" style="text-align: center;" href="#" target="_blank" role="button">Learn More</a>
+                  {{--  <a class="btn btn-primary btn-lg" style="text-align: center;" href="#" target="_blank" role="button">Learn More</a>--}}
                 </div>
             </div>
             <div class="col-md-4"></div>
